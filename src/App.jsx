558ded26e01da43d8770874003580cb23ca7be05
sorbari2016 <152311585+Sorbari2016@users.noreply.github.com>
2026-08-18 00:38:11 +0100

    Create Main component fragment, Hero component, & styled

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Header from "./components/Header.jsx";
+import Main from "./components/Main.jsx";
 import "./styles/general.css";
 import "./styles/header.css";
+import "./styles/main.css";
 
 function App() {
   return (
     <>
       <Header />
+      <Main />
     </>
   );
 }
