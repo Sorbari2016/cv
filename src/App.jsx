9f85f51d47f8d36ea34fce77504e624e358442a2
sorbari2016 <152311585+Sorbari2016@users.noreply.github.com>
2026-08-17 22:47:02 +0100

    Create header, style general and header

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header.jsx";
+import "./styles/general.css";
+import "./styles/header.css";
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
